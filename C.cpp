9fc0049f479e222fdcccb0c8f689659b35d76b26
Shin <stariver0812@gmail.com>
2019-03-11 21:33:41 +0900

    Modified C.cpp

--- C.cpp
+++ C.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string s;
+    cin >> s;
+    int cnt0 = 0;
+    int cnt1 = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '0') cnt0++;
+        if (s[i] == '1') cnt1++;
+    }
+    cout << min(cnt0, cnt1) * 2 << endl;
 }
